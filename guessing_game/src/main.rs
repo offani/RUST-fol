8ecf15ab3f9715450f9cdc4299ce484eca770c8f
“Aniketh <aniketh1905@gmail.com>
2024-01-07 00:55:58 +0530

    fiinal game

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+
+use std::io; //standard io librabry 
+fn main(){
+    println!("Geussing game start");
+    println!("Please input your guess. ");
+
+    let mut guess = String::new();  //mutabale variable having str value 
+    io::stdin()
+        .read_line(&mut guess) //input to read_line function and safe reference (&)to mut guess 
+        .expect("Failed to read line");  // readline return a value ok or err to handle err we write expect cand be handled in a better way
+    println!("You have guessed :{guess}") //{} like f strings in py  for passing variable
+
+
+
 }
---
+++ hello_cargo/target/debug/deps/hello_cargo-3b684416ab10d5b8.d
@@ -0,0 +1,5 @@
+D:\RUST fol\hello_cargo\target\debug\deps\libhello_cargo-3b684416ab10d5b8.rmeta: src\main.rs
+
+D:\RUST fol\hello_cargo\target\debug\deps\hello_cargo-3b684416ab10d5b8.d: src\main.rs
+
+src\main.rs:
